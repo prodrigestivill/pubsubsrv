265adf74a8531af9c97017bc9c34a5fec4f293c5
prodrigestivill <prodrigestivill@02e4dd20-ba62-11de-b431-8336cfaced44>
2009-10-17 01:35:33 +0000

    Added simple text mode. Separated SMTP protcol.

--- trunk/src/protocol/smtp.c
+++ trunk/src/protocol/smtp.c
@@ -1,14 +1,14 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * text.c
+ * smtp.c
  * Copyright (C) Pau Rodriguez 2009 <prodrigestivill@gmail.com>
  * 
- * text.c is free software: you can redistribute it and/or modify it
+ * smtp.c is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * text.c is distributed in the hope that it will be useful, but
+ * smtp.c is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -23,7 +23,7 @@
 #include <unistd.h>
 #include <string.h>
 
-void protocol_text_input(struct client *from, char buf[], int len){
+void protocol_smtp_input(struct client *from, char buf[], int len){
 	struct subscriber *s;
     struct publisher *p;
 	int r;
@@ -32,27 +32,47 @@ void protocol_text_input(struct client *from, char buf[], int len){
 			//Remove tailing '\n'
 			while(len>1 && (buf[len-1]=='\n' || buf[len-1]=='\r'))
 				len--;
-            if (len > 10 && strncmp("SUBSCRIBE ", buf, 10)==0){
+			//RECV FROM:
+            if (len > 10 && strncmp("RECV FROM:", buf, 10)==0){
 				s = subscriber(from, get_topic(buf+10, len-10));
+				r = write(from->connection, "250 Subscribed\r\n", 16);
 				s->state = 1;
 				break;
 			}
-		    if (len > 8 && strncmp("PUBLISH ", buf, 8)==0){
+            //RCPT TO:
+		    if (len > 8 && strncmp("RCPT TO:", buf, 8)==0){
 				p = publisher(from, get_topic(buf+8, len-8));
+				r = write(from->connection, "250 Publisher set\r\n", 19);
 				p->state = 1;
 				break;
 			}
-		    if (len > 4 && strncmp("START", buf, 5)==0){
+            //DATA
+		    if (len > 3 && strncmp("DATA", buf, 4)==0){
+				r = write(from->connection, "354 End data with <CR><LF>.<CR><LF>\r\n", 37);
 				from->state = 1;
 				break;
 			}
+		    //QUIT
 		    if (len > 3 && strncmp("QUIT", buf, 4)==0){
 				remove_client(from);
+				r = write(from->connection, "221 Bye\r\n", 9);
 				server_close(from->connection);
 				break;
 			}
-			r = write(from->connection, "COMMANDS: SUBSCRIBE,PUBLISH,START,QUIT\r\n", 40);
+		    //HELO
+		    if (len > 3 && strncmp("HELO", buf, 4)==0){
+				r = write(from->connection, "250 Helo\r\n", 10);
+				break;
+			}
+			r = write(from->connection, "502 Command not implemented\r\n", 29);
         	break;
+		case 2: //Streaming (last ended \n)
+			if (len>0 && buf[0]=='.' && (len==2 || (len==3 && buf[1]=='\r'))){
+				from->state = 0;
+				r = write(from->connection, "250 Ok\r\n", 8);
+				break;
+			}
+			//Don't break here!!!
         case 1: // Streaming mode
             client_list_for_each(p, &from->publishers){
 				if (p->state>0){
@@ -63,7 +83,7 @@ void protocol_text_input(struct client *from, char buf[], int len){
     }
 }
  
-int protocol_text_read(struct client *c){
+int protocol_smtp_read(struct client *c){
     char buf[LINE_MAX_LEN];
 	int i, l, n, len;
 	len = read(c->connection, buf, LINE_MAX_LEN);
@@ -71,35 +91,23 @@ int protocol_text_read(struct client *c){
 	l = 0;
 	for (i=0; i<len; i++){
 		if (buf[i] == '\n'){
-			protocol_text_input(c, &buf[l], i-l+1);
+			protocol_smtp_input(c, &buf[l], i-l+1);
+			if (c->state==1)
+				c->state = 2;
 			l=i+1;
 			n++;
 		}
 	}
 	if (l<len){
-		protocol_text_input(c, &buf[l], len-l);
+		protocol_smtp_input(c, &buf[l], len-l);
 		n++;
+		if (c->state==2)
+			c->state = 1;
 	}
     return n;
-   	//TODO permit char-by-char commands?.
-    /*
-    n = 0;
-    len = 0;
-    do{
-        c = &buf[len++];
-        r = read(c->connection, c, 1);
-        if (r==1 && *c == '\n'){
-            protocol_text_input(c, buf, len);
-            n++;
-            len = 0;
-        }
-    }while(r>0 && len<LINE_MAX_LEN);
-    if (r<0)
-        return -1;
-	*/
 }
 
-int protocol_text_write
+int protocol_smtp_write
 	(struct client *from, struct client *to, char buf[], int len){
 	int r;
 	if (from!=to && to->state>0){
@@ -112,24 +120,26 @@ int protocol_text_write
 		return 0;
 }
 
-struct client *protocol_text_newclient(int fd){
+struct client *protocol_smtp_newclient(int fd){
+	int r;
+	r = write(fd, "220 PubSub Server\r\n", 19);
 	return get_client(fd);
 }
 
-void protocol_text_endclient(struct client* c){
+void protocol_smtp_endclient(struct client* c){
 }
 
-void protocol_text_free_client_data(struct client* c){
+void protocol_smtp_free_client_data(struct client* c){
 }
 
-void protocol_text_free_topic_data(struct topic* t){
+void protocol_smtp_free_topic_data(struct topic* t){
 }
 
-void protocol_text(){
-    server_read = protocol_text_read;
-	server_write = protocol_text_write;
-	server_newclient = protocol_text_newclient;
-	server_endclient = protocol_text_endclient;
-	free_client_data = protocol_text_free_client_data;
-	free_topic_data = protocol_text_free_topic_data;
+void protocol_smtp(){
+    server_read = protocol_smtp_read;
+	server_write = protocol_smtp_write;
+	server_newclient = protocol_smtp_newclient;
+	server_endclient = protocol_smtp_endclient;
+	free_client_data = protocol_smtp_free_client_data;
+	free_topic_data = protocol_smtp_free_topic_data;
 }
